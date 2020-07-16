a1600cd95443d1616253618838f790af6da13c2c
wangchen <597358816@qq.com>
2020-07-16 20:54:37 +0800

    实现RemoteController

---
+++ myspider/movie-edge-service/src/main/java/com/test/movie/echarts/ScoreData.java
@@ -0,0 +1,11 @@
+package com.test.movie.echarts;
+
+public class ScoreData {
+    String Score;
+    int count;
+
+    public ScoreData(String score, int count) {
+        Score = score;
+        this.count = count;
+    }
+}
